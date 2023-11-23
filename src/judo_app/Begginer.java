98e61889420ca9aafed0114edab7e097e9313686
Dima Alhafez <111871337+dima4501@users.noreply.github.com>
2023-11-23 12:22:08 +0100

    Add files via upload

---
+++ src/judo_app/Begginer.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package judo_app;
+
+/**
+ *
+ * @author Dima
+ */
+public class Begginer {
+    private String name;
+    private double weight;
+    private int private_lesson;
+    private String weight_category;
+    private double cost;
+    public Begginer(String n,double w,int pclass)
+    {
+        name=n;
+        weight=w;
+        private_lesson=pclass;
+    }
+    public String cal_weightcategory()
+    {
+        if(weight<=66)
+            weight_category="Flyweight";
+        else if(weight<=73)
+            weight_category="Lightweight";
+        else if(weight<=81)
+            weight_category="Light Middle weight";
+        else if(weight<=90)
+            weight_category="Middle weight";
+        else if(weight<=100)
+            weight_category="Light Heavy weight";
+        else
+            weight_category="Heavy weight";
+        return weight_category;
+    }
+    public double calc_cost()
+    {
+        double training,plesson;
+        training=4*25;
+        plesson=private_lesson*9.50;
+        cost=training+plesson;
+        return cost;
+    }
+    
+    
+    
+}
---
+++ src/judo_app/Judo_App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package judo_app;
+
+/**
+ *
+ * @author Dima
+ */
+public class Judo_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Home_Page p1 = new Home_Page();
+        p1.show();
+    }
+
+}
